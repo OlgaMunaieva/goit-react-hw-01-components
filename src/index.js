46428523149c17beba2dfc20518d853ee27c97f2
OlgaMunaieva <asyssync@gmail.com>
2023-03-19 11:51:22 +0200

    initial data and profile markup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
 import './index.css';
+import { Profile } from 'components/profile/Profile';
+import user from './components/profile/user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile user={user} />
   </React.StrictMode>
 );
