a129181666c464d1c6bcb5b3f34edbefd17c9d57
OlgaMunaieva <asyssync@gmail.com>
2023-03-20 22:18:52 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Profile } from 'components/profile/Profile';
-import user from './components/profile/user.json';
-import { Statistics } from 'components/statistics/Statistics';
-import data from './components/statistics/data.json';
-import { FriendList } from 'components/friends/Friends';
-import friends from './components/friends/FriendList.json';
-import { TransactionHistory } from 'components/transactions/TransactionHistory';
-import transactions from './components/transactions/transactions.json';
+import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Profile user={user} />
-    <Statistics title="Upload stats" stats={data} />
-    <Statistics stats={data} />
-    <FriendList friends={friends} />
-    <TransactionHistory items={transactions} />
+    <App />
   </React.StrictMode>
 );
